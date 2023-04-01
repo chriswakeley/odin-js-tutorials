0242b9a6e0f0dcd477ce5807625cd93083832bbd
chriswakeley <chriswakeley@gmail.com>
2023-04-01 14:40:12 -0400

    Complete chapter 1

---
+++ fundamentals-part1/script.js
@@ -0,0 +1,26 @@
+/*
+let admin; 
+let name;
+
+name = "John";
+admin = name;
+alert(admin);
+
+let ourPlanetName = "earth";
+let currentUserName;
+*/
+let a = 10;
+
+console.log(23 + 97);
+console.log(1 + 2 + 3 + 4 + 5 + 6);
+console.log((4+6+9)/77);
+
+console.log(a);
+9*a;
+let b = 7*a;
+console.log(b);
+
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+console.log(percentage);
